c6ab01a38c4f608ecae8601a95ceac595a8226cb
miguel-enrique <miguelxzsmaster@gmail.com>
2020-03-30 20:32:14 -0300

    exercicio do nome completo

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Nomecompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva seu primeiro nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Agora digite seu sobrenome: ");
+            string sobrenome = Console.ReadLine();
+            Console.WriteLine("Entao seu nome é " + nome + " " + sobrenome);
+            Console.WriteLine("Então seu nome em catalogo é assim " + sobrenome.ToUpper() + ", " + nome);
+
+            Console.ReadKey();
+        }
+    }
+}
